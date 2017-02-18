a95fdb4b2fe25efddd0a98335ce8ab5762f05aae
audiosprite <jpfiorilla@gmail.com>
2017-02-18 12:56:23 -0500

    wordsapi

--- src/modules/hitapi.js
+++ src/modules/hitapi.js
@@ -1,6 +1,9 @@
 const rp = require('request-promise');
+const rita = require('rita').RiTa;
+
 const guardianKey = require('./guardiankey');
 const nytKey = require('./nytkey');
+const wordsKey = require('./wordskey');
 
 let guardianUSNewsSearch = 'https://content.guardianapis.com/search?section=us-news&api-key=' + guardianKey;
 let guardianSectionEndpoint = 'https://content.guardianapis.com/sections?api-key=' + guardianKey;
@@ -16,4 +19,20 @@ const guardianUSsearch = function(){
     })
 }
 
-guardianUSsearch();
+// guardianUSsearch();
+
+const wordsAPI = function(word){
+    let options = {
+        uri: 'https://wordsapiv1.p.mashape.com/words/' + word + '/similarTo',
+        headers: {
+            "X-Mashape-Key": wordsKey,
+            "Accept": "application/json"
+        }
+    }
+    rp(options)
+    .then(res => {
+        console.log(res)
+    })
+}
+
+wordsAPI('conservative');
